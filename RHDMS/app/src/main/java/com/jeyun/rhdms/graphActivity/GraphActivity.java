c5d191a7beb809d3835db0e40933f48306c7dee9
sdk <kimseong124@gmail.com>
2024-10-11 21:11:44 +0900

    Add Calendar

--- RHDMS/app/src/main/java/com/jeyun/rhdms/graphActivity/GraphActivity.java
+++ RHDMS/app/src/main/java/com/jeyun/rhdms/graphActivity/GraphActivity.java
@@ -5,12 +5,14 @@ import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.jeyun.rhdms.databinding.ActivityPillInfoBinding;
+import com.jeyun.rhdms.util.CustomCalendar;
+import com.jeyun.rhdms.util.MyCalendar;
 
 public abstract class GraphActivity<T> extends AppCompatActivity
 {
     protected ActivityPillInfoBinding binding;
-
     protected String title;
+
     protected void create()
     {
         binding = ActivityPillInfoBinding.inflate(getLayoutInflater());
