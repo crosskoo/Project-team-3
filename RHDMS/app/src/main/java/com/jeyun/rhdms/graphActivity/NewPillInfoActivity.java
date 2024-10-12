c993e3c1c225a541d5e6dd3291d6b9573250af7a
sdk <kimseong124@gmail.com>
2024-10-12 13:25:00 +0900

    update NewPillInfoActivity

--- RHDMS/app/src/main/java/com/jeyun/rhdms/graphActivity/NewPillInfoActivity.java
+++ RHDMS/app/src/main/java/com/jeyun/rhdms/graphActivity/NewPillInfoActivity.java
@@ -3,13 +3,26 @@ package com.jeyun.rhdms.graphActivity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import androidx.annotation.Nullable;
+import com.jeyun.rhdms.fragment.TimeFragment;
+import com.jeyun.rhdms.handler.PillHandler;
+import com.jeyun.rhdms.handler.entity.Pill;
 import com.jeyun.rhdms.handler.entity.wrapper.PillBox;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NewPillInfoActivity extends GraphActivity<PillBox>
 {
     public NewPillInfoActivity()
     {
         title = "복약 정보";
+        supplier = TimeFragment::new;
+        function = isWeek ->
+        {
+            PillHandler ph = new PillHandler();
+            List<Pill> pills = isWeek ? ph.getDataInWeek(calendar.timeNow) : ph.getDataInMonth(calendar.timeNow);
+            return new PillBox((ArrayList<Pill>) pills);
+        }; // 복약 정보를 불러오는 함수
     }
 
     @Override
