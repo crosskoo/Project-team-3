960f644e57afb4b0357b765fbfe466f821f48c15
V1v1p <120006486+V1v1p@users.noreply.github.com>
2024-10-16 00:31:33 +0900

    Update SugarInfoActivity.java

--- RHDMS/app/src/main/java/com/jeyun/rhdms/graphActivity/SugarInfoActivity.java
+++ RHDMS/app/src/main/java/com/jeyun/rhdms/graphActivity/SugarInfoActivity.java
@@ -4,7 +4,12 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.jeyun.rhdms.fragment.BarFragment;
+import com.jeyun.rhdms.handler.BloodHandler;
+import com.jeyun.rhdms.handler.entity.Blood;
 import com.jeyun.rhdms.handler.entity.wrapper.BloodPack;
 
 public class SugarInfoActivity extends GraphActivity<BloodPack>
@@ -14,6 +19,19 @@ public class SugarInfoActivity extends GraphActivity<BloodPack>
         final String type = "31";
 
         title = "혈당 정보";
+        supplier = () -> new BarFragment(type);
+        function = isWeek ->
+        {
+            BloodHandler dh = new BloodHandler(type);
+            List<Blood> dataset =
+                    isWeek ?
+                            dh.getDataInWeek(calendar.timeNow) :
+                            dh.getDataInMonth(calendar.timeNow);
+
+            System.out.println("TEST : " + dataset);
+            if(dataset.isEmpty()) return new BloodPack(new ArrayList<>());
+            return new BloodPack((ArrayList<Blood>) dataset);
+        };
     }
 
     @Override
