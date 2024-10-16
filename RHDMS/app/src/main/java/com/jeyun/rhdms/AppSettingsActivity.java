732afd2f5cc4a94414c4dd9f69439b73e3503120
kookyowoong <161264814+crosskoo@users.noreply.github.com>
2024-10-16 22:27:28 +0900

    Activity Update

---
+++ RHDMS/app/src/main/java/com/jeyun/rhdms/AppSettingsActivity.java
@@ -0,0 +1,15 @@
+package com.jeyun.rhdms;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AppSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_settings);
+
+        // Initialize your settings UI components here
+    }
+}
--- RHDMS/app/src/main/java/com/jeyun/rhdms/MenuActivity.java
+++ RHDMS/app/src/main/java/com/jeyun/rhdms/MenuActivity.java
@@ -40,7 +40,7 @@ public class MenuActivity extends AppCompatActivity {
         binding.buttonPressureInfo.setOnClickListener(v -> switchActivity(PressureInfoActivity.class));
         binding.buttonSugarInfo.setOnClickListener(v -> switchActivity(SugarInfoActivity.class));
         binding.buttonBle.setOnClickListener(v -> switchActivity(BleActivity.class));
-        binding.buttonSettings.setOnClickListener(v -> switchActivity(DeviceSettingsActivity.class)); //설정 관련해서 추가.
+        binding.buttonSettings.setOnClickListener(v -> switchActivity(SettingMenuActivity.class));  //설정 관련해서 추가.
     }
 
     private <T> void switchActivity(T cls)
