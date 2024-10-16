cfa8ac52808cd9640baae869298fdebcb47eadcf
kookyowoong <161264814+crosskoo@users.noreply.github.com>
2024-10-16 21:09:52 +0900

    Setting -> Device_Setting

--- RHDMS/app/src/main/java/com/jeyun/rhdms/MenuActivity.java
+++ RHDMS/app/src/main/java/com/jeyun/rhdms/MenuActivity.java
@@ -14,8 +14,6 @@ import com.jeyun.rhdms.graphActivity.NewPillInfoActivity;
 import com.jeyun.rhdms.util.worker.Inspector;
 import com.jeyun.rhdms.util.worker.MyWorkManager;
 
-import java.util.concurrent.TimeUnit;
-
 public class MenuActivity extends AppCompatActivity {
 
     private ActivityMenuBinding binding;
@@ -42,7 +40,7 @@ public class MenuActivity extends AppCompatActivity {
         binding.buttonPressureInfo.setOnClickListener(v -> switchActivity(PressureInfoActivity.class));
         binding.buttonSugarInfo.setOnClickListener(v -> switchActivity(SugarInfoActivity.class));
         binding.buttonBle.setOnClickListener(v -> switchActivity(BleActivity.class));
-        binding.buttonSettings.setOnClickListener(v -> switchActivity(SettingsActivity.class)); //설정 관련해서 추가.
+        binding.buttonSettings.setOnClickListener(v -> switchActivity(DeviceSettingsActivity.class)); //설정 관련해서 추가.
     }
 
     private <T> void switchActivity(T cls)
