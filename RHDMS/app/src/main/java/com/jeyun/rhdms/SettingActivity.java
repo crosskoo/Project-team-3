2afbf07fec608a7083648655b4bbd7776227e809
sdk <kimseong124@gmail.com>
2024-11-16 11:48:11 +0900

    create  SettingActivity

---
+++ RHDMS/app/src/main/java/com/jeyun/rhdms/SettingActivity.java
@@ -0,0 +1,25 @@
+package com.jeyun.rhdms;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.jeyun.rhdms.databinding.ActivitySettingBinding;
+
+public class SettingActivity extends AppCompatActivity
+{
+    private ActivitySettingBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivitySettingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
